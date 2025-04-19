dac8c91770dcd57c56d64b7834f75bd1b67733d8
Felix <felix.antony168@gmail.com>
2025-04-19 17:04:23 +0800

    removed unused function import

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,10 +1,11 @@
 from weaviate_db import Database
 from dotenv import load_dotenv
 import pandas as pd
-from os import getenv
 
 if __name__ == '__main__':
-    load_dotenv(dotenv_path=".env")
+    load_dotenv(dotenv_path=".env") # To access Weaviate Database
     dataset = pd.read_parquet("Embeddings.parquet")
     with Database() as db:
         db.ingest_data(dataset)
+    print("Success ingesting data")
+    
